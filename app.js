882d35fefa10ed0b5bfb5851ae4c19e93e081fc8
Ekant-Yash-Kashyap <yash.kashyap@ekant.in>
2025-06-15 12:51:49 +0530

    changes

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const express = require("express");
 // Local Module
 const userRouter = require("./routes/userRouter");
 const hostRouter = require("./routes/hostRouter");
+const rootDir = require("./utils/pathUtil");
 
 const app = express();
 
@@ -13,7 +14,7 @@ app.use(userRouter);
 app.use("/host", hostRouter);
 
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname,"views", "404.html"));
+  res.status(404).sendFile(path.join(rootDir,"views", "404.html"));
 });
 
 const PORT = 3000;
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -4,8 +4,12 @@ const path = require('path')
 const express = require("express");
 const userRouter = express.Router();
 
+// Local Module
+const rootDir = require("../utils/pathUtil");
+
+
 userRouter.get("/",(req, res, next) => {
-  res.sendFile(path.join(__dirname, '../', 'views', 'home.html'))
+  res.sendFile(path.join(rootDir, 'views', 'home.html'))
 });
 
 module.exports = userRouter;
